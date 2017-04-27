ad18d41993a304da377c1566d7944d184bc632c4
Tobias Fuchs <fuchst@nm.ifi.lmu.de>
2017-04-27 04:45:04 +0200

    CMake build boilerplate

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ BUILD_DIR=./build
 export DASH_BASE=${HOME}/opt/dash-0.3.0
 export DART_BASE=$DASH_BASE
 
+if [ "${PAPI_HOME}" = "" ]; then
+  PAPI_HOME=$PAPI_BASE
+fi
 
 mkdir -p $BUILD_DIR
 rm -Rf $BUILD_DIR/*
@@ -13,6 +16,8 @@ rm -Rf $BUILD_DIR/*
         -DCMAKE_INSTALL_PREFIX=$BUILD_DIR/install/ \
         \
         -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+        \
+        -DPAPI_PREFIX=${PAPI_HOME} \
   .. && \
   make install)
 
--- dylocxx/src/init.cc
+++ dylocxx/src/init.cc
@@ -9,21 +9,17 @@ namespace dyloc {
 static bool _is_initialized = false;
 
 void init(int * argc, char *** argv) {
-  if (dyloc::is_initialized() ||
+  if (!dyloc::is_initialized() &&
       dart_init(argc, argv) == DART_OK) {
     _is_initialized = true;
-    return DYLOC_OK;
   }
-  return DYLOC_ERR_OTHER;
 }
 
 void finalize() {
-  if (!dyloc::is_initialized() ||
+  if (dyloc::is_initialized() &&
       dart_exit() == DART_OK) {
     _is_initialized = false;
-    return DYLOC_OK;
   }
-  return DYLOC_ERR_OTHER;
 }
 
 bool is_initialized() {
