8b73eb6ce63e26e9ac2501a228b196bf27aa42ad
jakubo87 <jakub_o@hotmail.com>
2019-09-30 04:06:17 +0200

    first changes to project: adding add_distance_metric and tests

--- dylocxx/test/topology_test.cc
+++ dylocxx/test/topology_test.cc
@@ -143,6 +143,23 @@ TEST_F(TopologyTest, ScopeDomains) {
   dyloc::finalize();
 }
 
+//Jakub
+TEST_F(TopologyTest, DistanceMetric) {
+  dyloc::init(&TESTENV.argc, &TESTENV.argv);
+  auto & topo = dyloc::team_topology();
+  DYLOC_LOG_DEBUG("TopologyTest.DistanceMetric", "Total Distance:");
+//TODO
+  
+  auto dist_fn= [](int a){ return 42; };
+  std::string str="TestMetric";
+  topo.add_distance_metric(str, dist_fn);
+  ASSERT_EQ(0, str.compare(topo.list_distance_metrics()));
+
+  dyloc::finalize();
+}
+
+
+
 } // namespace dyloc
 } // namespace test
 
